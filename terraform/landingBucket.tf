07df8cf4dc53f1a3190e84ea04da7b1432eb8b24
Reyur Mathura <Reyur.Mathura@equalexperts.com>
2024-11-25 15:47:45 +0200

    init

---
+++ terraform/landingBucket.tf
@@ -0,0 +1,29 @@
+# Provider configuration 
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "ee-india-se-data"
+  region  = "US"
+}
+
+# Define the GCS bucket resource
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "landing_bucket" {
+  name                        = "se-data-landing-reyur"
+  location                    = "us-central1"
+  storage_class               = "STANDARD"
+  force_destroy               = false
+
+  # Manage access to objects in bucket at global level
+  uniform_bucket_level_access = true
+}
+
+# Define IAM policy bindings to make the bucket private
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket_iam
+resource "google_storage_bucket_iam_binding" "no_public_access" {
+  bucket = google_storage_bucket.landing_bucket.name
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "projectOwner:ee-india-se-data"
+  ]
+}
