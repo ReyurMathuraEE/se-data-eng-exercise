cfbd9c3800f39bb889a61aff2705dcd5c03e7326
Reyur Mathura <Reyur.Mathura@equalexperts.com>
2025-01-13 18:18:58 +0200

    no message

--- terraform/snowflake/provider.tf
+++ terraform/snowflake/provider.tf
@@ -7,12 +7,12 @@ terraform {
 }
 
 provider "snowflake" {
-  #account   = var.snowflake_account
-  #username  = var.snowflake_username
-  #password  = var.snowflake_password
-  #role      = var.snowflake_role
-  #warehouse = var.snowflake_warehouse
-  #region    = var.snowflake_region
-  #database  = var.snowflake_database
-  #schema    = var.snowflake_schema
+  account   = var.snowflake_account
+  username  = var.snowflake_username
+  password  = var.snowflake_password
+  role      = var.snowflake_role
+  warehouse = var.snowflake_warehouse
+  region    = var.snowflake_region
+  database  = var.snowflake_database
+  schema    = var.snowflake_schema
 }
