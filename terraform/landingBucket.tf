f4a4e0c985910ac403659770c8dd917417d98030
Reyur Mathura <Reyur.Mathura@equalexperts.com>
2025-01-30 13:22:13 +0200

    Update bucket

--- terraform/landingBucket.tf
+++ terraform/landingBucket.tf
@@ -7,23 +7,8 @@ provider "google" {
 
 # Define the GCS bucket resource
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
-resource "google_storage_bucket" "landing_bucket" {
-  name                        = "ee-se-data-engg/reyur"
-  location                    = "us-central1"
-  storage_class               = "STANDARD"
-  force_destroy               = false
-
-  # Manage access to objects in bucket at global level
-  uniform_bucket_level_access = true
-}
-
-# Define IAM policy bindings to make the bucket private
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket_iam
-resource "google_storage_bucket_iam_binding" "no_public_access" {
-  bucket = google_storage_bucket.landing_bucket.name
-  role   = "roles/storage.objectViewer"
-
-  members = [
-    "projectOwner:ee-india-se-data"
-  ]
+resource "google_storage_bucket_object" "content_folder" {
+  name          = "reyur/"
+  content       = "Not really a directory, but it's empty."
+  bucket        = "ee-se-data-engg"
 }
