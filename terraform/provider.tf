d0fa93437f7b5fb0153cd2dbb35770369079dee0
Reyur Mathura <Reyur.Mathura@equalexperts.com>
2025-02-01 14:57:55 +0200

    Try using file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,7 +18,7 @@ provider "snowflake" {
   organization_name = var.organization_name
   account_name      = var.account_name
   user              = var.user
-  private_key            = var.private_key
+  private_key       = file(var.private_key)
 
   preview_features_enabled = ["snowflake_table_resource"]
 }
