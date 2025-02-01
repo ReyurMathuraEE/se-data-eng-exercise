ee9516254fb532c4252ea3705df02cad071e6248
Reyur Mathura <Reyur.Mathura@equalexperts.com>
2025-02-01 14:45:12 +0200

    remove jwt auth

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,7 +18,6 @@ provider "snowflake" {
   organization_name = var.organization_name
   account_name      = var.account_name
   user              = var.user
-  authenticator          = "SNOWFLAKE_JWT"
   private_key            = var.private_key
 
   preview_features_enabled = ["snowflake_table_resource"]
